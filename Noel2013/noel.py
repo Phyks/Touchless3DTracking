4d96e938c64521fec0005fb504bbe7e852375a48
hackEns <hackens@clipper.ens.fr>
2013-12-14 02:13:34 +0100

    Working on code for Noel2013
    Started serial handling
    
    TODO : Serial in main loop + args

--- Noel2013/noel.py
+++ Noel2013/noel.py
@@ -21,6 +21,9 @@
 from sound4python import sound
 from multiprocessing import Process
 import math
+import serial
+import sys
+import getopt
 
 
 class _Getch:
@@ -46,45 +49,109 @@ def play_wave(frequency=440, nb_secs=1.):
     sine_wave = []
     for i in range(math.ceil(nb_secs*framerate)+1):
         sine_wave.append(int(16384*math.sin(2*math.pi*frequency*i/framerate)))
-
     sound(sine_wave)
+
+
+def read_serial(ser):
+    line = ser.readline()
+    line = line.decode().strip("\r\n")
+    line = line.split(" ")
+    return [int(i or 0) for i in line]
 
 framerate = 16000
 processes = []
-frequency = 440
+frequencies = {"FA": 698.5, "SOL": 784, "LA": 880, "SIb": 932, "DO": 1046.5}
+notes = frequencies.keys()
+thresholds = []
+measures = []
 
+serial_port = "/dev/ttyACM0"
+serial_speed = 115200
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "hs:", ["help", "serial="])
+
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print("Touchless 3D tracking with color mapping")
+            print("\nUsage : "+sys.argv[0]+" [OPTIONS]")
+            print("\nTrack the position of your hand in 3D and map it " +
+                  "in RGB space")
+            print("\nOptions :")
+            print("\t-h (--help) \t display this help message")
+            print("\t-s (--serial=) \t change serial port (default is " +
+                  "/dev/tty/ACM0")
+            sys.exit(0)
+        elif opt in ("-s", "--serial"):
+            serial_port = arg
+except getopt.GetoptError:
+    pass
 
 # Handle serial opening
-# *** TODO ***
+try:
+    ser = serial.Serial(serial_port, serial_speed)
+except Exception as e:
+    sys.exit("Invalid serial port options : "+str(e))
 
+try:
+    ser.open()
+except Exception as e:
+    sys.exit("Error while opening serial port : "+str(e))
+
+if not ser.isOpen():
+    sys.exit("Serial port not opened")
+
+ser.flushInput()
+ser.flushOutput()
 
 # Handle calibration
-# *** TODO ***
+for i in len(notes):
+    print("Calibrating note "+notes[i])
+    print("Touch and release the key now... Will get 10k samples.")
+
+    for sample in range(10000):
+        measures[sample] = read_serial(ser)
+
+    measures.sort()
+    max_diff = 0
+    max_diff_index = 0
+
+    for sample in range(10000-1):
+        if measures[sample+1]-measures[sample]:
+            max_diff = measures[sample+1]-measures[sample]
+            max_diff_index = sample
+
+    thresholds[i] = (measures[max_diff_index+1]+measures[max_diff_index])/2
 
 # Main loop
 # *** TODO *** : Add fetch from the serial also
 print("Running... Press q to quit.")
 running = True
 while running:
+    serial_input = read_serial(ser)
+    
+    for note in range(notes):
+        
+    
     char = getch()
     if char == "q":
         print("Exiting...")
         running = False
         continue
-    elif char == "a":
-        frequency = 440
-    elif char == "b":
-        frequency = 493.88
-    elif char == "c":
-        frequency = 523.25
-    elif char == "d":
-        frequency = 587.33
-    elif char == "e":
-        frequency = 659.26
-    elif char == "f":
-        frequency = 698.46
-    elif char == "g":
-        frequency = 783.99
+    if char == "a":
+        frequency = frequencies[0]
+    if char == "b":
+        frequency = frequencies[1]
+    if char == "c":
+        frequency = frequencies[2]
+    if char == "d":
+        frequency = frequencies[3]
+    if char == "e":
+        frequency = frequencies[4]
+    if char == "f":
+        frequency = frequencies[5]
+    if char == "g":
+        frequency = frequencies[6]
     else:
         continue
 
